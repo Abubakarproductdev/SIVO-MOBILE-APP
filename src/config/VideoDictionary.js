58d8d1a856451f9e0af9cc73fbd728849dd51097
Muhammad Abubakar <sshaiy2255@gmail.com>
2026-04-25 23:47:48 +0500

    chore: update mobile app changes

---
+++ src/config/VideoDictionary.js
@@ -0,0 +1,23 @@
+// VideoDictionary.js
+export const VideoDictionary = {
+  boss: require('../../assets/videos1/boss.mp4'),
+  call: require('../../assets/videos1/call.mp4'),
+  client: require('../../assets/videos1/client.mp4'),
+  come: require('../../assets/videos1/come.mp4'),
+  day: require('../../assets/videos1/day.mp4'),
+  female: require('../../assets/videos1/female.mp4'),
+  give: require('../../assets/videos1/give.mp4'),
+  i: require('../../assets/videos1/I.mp4'),
+  idea: require('../../assets/videos1/idea.mp4'),
+  love: require('../../assets/videos1/love.mp4'),
+  meet: require('../../assets/videos1/meet.mp4'),
+  plan: require('../../assets/videos1/plan.mp4'),
+  project: require('../../assets/videos1/project.mp4'),
+  read: require('../../assets/videos1/read.mp4'),
+  report: require('../../assets/videos1/report.mp4'),
+  this: require('../../assets/videos1/this.mp4'),
+  time: require('../../assets/videos1/time.mp4'),
+  today: require('../../assets/videos1/today.mp4'),
+  want: require('../../assets/videos1/want.mp4'),
+  you: require('../../assets/videos1/you.mp4')
+};
